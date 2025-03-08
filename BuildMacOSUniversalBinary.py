db1632cfbb4a899230cb986fde294f0b5cfc6891
Nicolet <jmlee337@gmail.com>
2025-03-08 00:00:00 -0600

    Update BuildMacOSUniversalBinary.py

--- BuildMacOSUniversalBinary.py
+++ BuildMacOSUniversalBinary.py
@@ -322,7 +322,7 @@ def build(config):
                 "-DUSE_SYSTEM_ICONV=ON",
                 "-DUSE_SYSTEM_BZIP2=ON",
                 "-DUSE_SYSTEM_CURL=ON",
-                "-DSLIPPI_PLAYBACK=" + config["build_config"] == 'playback',
+                "-DSLIPPI_PLAYBACK=" + "true" if config["build_config"] == 'playback' else "false",
             ],
             env=env, cwd=arch)
 
